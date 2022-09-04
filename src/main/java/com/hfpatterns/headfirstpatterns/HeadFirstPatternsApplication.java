77acb90590c6116554a1da0d95b5f34d01a5b88a
vanderZet-dev <85804034+vanderDB@users.noreply.github.com>
2022-09-04 10:12:36 +0300

    initial

---
+++ src/main/java/com/hfpatterns/headfirstpatterns/HeadFirstPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.hfpatterns.headfirstpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadFirstPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadFirstPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hfpatterns/headfirstpatterns/HeadFirstPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfpatterns.headfirstpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadFirstPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
